e6fdca350e1e8ec10fb9ca1f388b5cd84ecfa927
Michał Talaga <michal.talaga.programming@gmail.com>
2024-08-29 12:56:34 +0200

    Initial Commit

---
+++ studentReminder/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  studentReminder
+//
+//  Created by Michał Talaga on 29/08/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
